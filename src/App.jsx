e595ea26d68be1bb6604abcc83db90098ec086fa
Jintops <jintops2003@gmail.com>
2025-07-07 15:01:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+       <h1 className="text-8xl text-amber-400">hellooo</h1>    </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
