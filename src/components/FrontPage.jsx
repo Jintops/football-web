4cf6fe6af6907f0b913533c218725dbc80b2d941
Jintops <jintops2003@gmail.com>
2025-07-08 14:09:28 +0530

    frontpage

---
+++ src/components/FrontPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FrontPage = () => {
+  return (
+    <div>
+        <div >
+           <h1>Elevate your </h1>
+           <span>soccer game</span>
+           <p>Professional soccer gear for players of all levels. From boots to jerseys, find everything you need to dominate the pitch.</p>
+           <div>
+            <button className='border bg-white hover:bg-green-200'>Shop Now</button>
+            <button>View Collections</button>
+           </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default FrontPage
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import FrontPage from './FrontPage'
 
 const Home = () => {
   return (
     <div>
         
+       <FrontPage/>
        
     </div>
 
