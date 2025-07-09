c255ed8e9b2355874f84b0108b164c640cde7cda
Jintops <jintops2003@gmail.com>
2025-07-09 14:21:12 +0530

    footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FrontPage from './FrontPage'
 import ProductPage from './ProductPage'
+import Footer from './Footer'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         
        <FrontPage/>
        <ProductPage/>
+       <Footer/>
     </div>
 
 
