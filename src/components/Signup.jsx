f13c333235c7931a554b252c50e65313d55acfd5
Jintops <jintops2003@gmail.com>
2025-07-26 18:51:34 +0530

    signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,34 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, redirect, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+    const [emailId ,setEmailId]=useState('');
+    const [password, setPassword]=useState('');
+    const [firstName,setFirstName]=useState('');
+  const navigate=useNavigate();
+
+
+const handleSignUP=async(e)=>{
+ try {
+    e.preventDefault();
+    const res = await axios.post(
+      "http://localhost:7777/signup",
+      {firstName, emailId, password },
+      { withCredentials: true }
+    );
+    console.log(res.data); 
+   navigate("/")
+  } catch (err) {
+    console.error("Login failed:", err.response?.data || err.message);
+     alert("Login failed");
+    
+  }
+}
   return (
       <div className="flex items-center justify-center min-h-screen ">
   <div className="bg-white shadow-lg rounded-2xl p-10 w-full max-w-md border border-gray-100">
     <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Sign Up</h2>
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSignUP}>
          <div>
         <label className="text-sm font-semibold text-gray-700">FirstName</label>
         <input
+        value={firstName}
           type="text"
           placeholder="Enter your Name"
           className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+          onChange={(e)=>setFirstName(e.target.value)}
         />
       </div>
       <div>
         <label className="text-sm font-semibold text-gray-700">Email</label>
         <input
+        value={emailId}
           type="email"
           placeholder="Enter your email"
           className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-        />
+        onChange={(e)=>setEmailId(e.target.value)}
+         />
       </div>
       <div>
         <label className="text-sm font-semibold text-gray-700">Password</label>
         <input
+        value={password}
           type="password"
           placeholder="Enter your password"
           className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+         onChange={(e)=>setPassword(e.target.value)}
         />
       </div>
       <button
