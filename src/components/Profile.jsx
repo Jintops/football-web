94becaab89f7736b00360de9f46f9b5101696f85
Jintops <jintops2003@gmail.com>
2025-08-01 15:30:59 +0530

    profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { BASE_URL } from '../utils/constants'
 
 const Profile = () => {
-    
+      const [data,setData]=useState([])
+    const profileData=async()=>{
+        try{
+         const res=await axios.get(BASE_URL+"profile",{withCredentials:true})
+         console.log(res.data.data)
+         setData(res.data.data)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{
+        profileData()
+    },[])
   return (
     <div>
+      {data.map((profile)=>(
         <div>
-
+         <h1>{profile.firstName}</h1>
+         <h1>lstName</h1>
+         <h3>gender</h3>
         </div>
+        ))}
     </div>
   )
 }
