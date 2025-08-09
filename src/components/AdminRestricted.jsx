35c3ff5ef59644217ed388d51a2b8f5e775a4beb
Jintops <jintops2003@gmail.com>
2025-08-09 14:57:36 +0530

    admin restriction to other pages

---
+++ src/components/AdminRestricted.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+
+const AdminRestricted = ({user,children}) => {
+    const location=useLocation();
+
+    if(user.role==="admin"){
+        return <Navigate to="/admin/overview" replace/>
+    }
+  return   <> {children}   </>
+  
+}
+
+export default AdminRestricted
