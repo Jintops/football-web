56df39aec906520e38003beb7206a7be80b0de1c
Jintops <jintops2003@gmail.com>
2025-08-09 16:53:20 +0530

    edit profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,42 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { BASE_URL } from '../utils/constants';
 import { Link } from 'react-router-dom';
 import { UserX } from 'lucide-react';
 const Profile = () => {
   const [data, setData] = useState(null);
-
+  const [openEdit,setOpenEdit]=useState(false)
+  const [firstName,setFirstName]=useState('')
+  const [lastName,setLastName]=useState('')
+  const [gender,setGender]=useState('')
   const profileData = async () => {
     try {
       const res = await axios.get(BASE_URL + 'profile', { withCredentials: true });
      
       setData(res.data.data);
+      setFirstName(res.data.data?.firstName || '');
+      setLastName(res.data.data?.lastName || '');
+      setGender(res.data.data?.gender || '');
     } catch (err) {
       console.log(err);
     }
   };
 
+  const handleEditProfile=async()=>{
+    try{
+    const res=await axios.put(BASE_URL+"profile/edit",{firstName,lastName,gender},{withCredentials:true});
+    await profileData();
+    setOpenEdit(false)
+  }catch(err){
+    console.log(err.message)
+  }
+  }
+
   useEffect(() => {
     profileData();
   }, []);
+
+
 if (!data) {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 px-4">
@@ -47,10 +65,12 @@ if (!data) {
           alt="Profile"
           className="w-20 h-20 rounded-full border-2 border-green-500 object-cover"
         />
+        
         <div>
           <h1 className="text-xl font-bold text-gray-800">{data.firstName} {data.lastName}</h1>
           <p className="text-sm text-gray-500 capitalize">Gender: {data.gender}</p>
         </div>
+     
       </div>
 
       <div className="grid grid-cols-2 gap-4 mt-4">
@@ -63,7 +83,24 @@ if (!data) {
           <p className="font-medium">{data.phone || 'N/A'}</p>
         </div>
       </div>
+          <button className=' bg-green-500 p-2 border border-green-600 rounded-lg w-32 text-white font-bold' onClick={()=>setOpenEdit(!openEdit)}>Edit</button>
       </div>
+
+
+   {openEdit &&<div className='border m-4'>
+     <h1 className='text-3xl text-center'>edit profile</h1>
+    <div className='m-4 flex flex-col mx-auto max-w-xl gap-4'>  
+
+      <img  src=''></img> 
+      <input className='border rounded-md p-2 w-40' value={firstName} type='text' onChange={(e)=>setFirstName(e.target.value)}></input>
+      <input className='border rounded-md p-2 w-40' value={lastName} type='text' onChange={(e)=>setLastName(e.target.value)}></input>
+    <input className='border rounded-md p-2 w-40' value={gender} type='text' onChange={(e)=>setGender(e.target.value)}></input>
+    <button className=' bg-green-500 p-2 border border-green-600 rounded-lg w-32 text-white font-bold' onClick={handleEditProfile}>Save </button>
+      </div>
+    </div>}
+
+
+
     </div>
   );
 };
