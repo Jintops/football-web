12831d6885ab74bfa8130f692326529b53e92e70
Jintops <jintops2003@gmail.com>
2025-08-30 16:14:06 +0530

    otp input backspace

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 
-const OTP_COUNT=4
+const OTP_COUNT=4;
 
 const Otp = () => {
 
@@ -14,12 +14,19 @@ const Otp = () => {
 
     const handleInput=(value,index)=>{
         if(isNaN(value)) return
+        const newValue=value.trim()
         const newArr=[...inputArr]
-        newArr[index]=value.slice(-1)
-        inputRef.current[index+1]?.focus()
+        newArr[index]=newValue.slice(-1)
         setInputArr(newArr)
+        newValue && inputRef.current[index+1]?.focus()
+    
     }
 
+    const handlekey=(e,index)=>{
+      if(!e.target.value && e.key==="Backspace"){
+        inputRef.current[index-1]?.focus()
+      }
+    }
   return (
     <div className='h-screen'>
         <div className='text-center'>Verify Otp</div>
@@ -27,7 +34,8 @@ const Otp = () => {
         {inputArr.map((input,index)=>(
          
           <input type='text' ref={(input)=>(inputRef.current[index]=input)} key={index} value={inputArr[index]} className='border w-20  p-2 m-2 text-center'
-          onChange={(e)=>handleInput(e.target.value,index)}>
+          onChange={(e)=>handleInput(e.target.value,index)}
+          onKeyDown={(e)=>handlekey(e,index)}>
           </input> 
 
        ))}
